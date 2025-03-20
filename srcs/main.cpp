bf833e020ea473466bc4e1a561d0068afef5ab06
Aida Neitenbach <aneitenb@c2r5p5.hive.fi>
2025-03-20 17:56:12 +0200

    basic Server class setup, simplified main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,72 +6,29 @@
 /*   By: aneitenb <aneitenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 12:50:45 by aneitenb          #+#    #+#             */
-/*   Updated: 2025/03/20 15:56:56 by aneitenb         ###   ########.fr       */
+/*   Updated: 2025/03/20 16:32:20 by aneitenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Webserv.hpp"
-#include "../includes/ConfigFile.hpp"
+#include "Webserv.hpp"
+#include "config/ConfigFile.hpp"
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[]) 
 {
-	if (argc != 2)
-	{
-		std::cerr << "Error: Wrong amount of parameters. Cannot initialize server." << std::endl;
-		return (1);
-	}
-
-	try
-	{
-		ConfigurationFile config;
-		
-		// Set configuration file path
-		if (argc == 1)
-		{
-			config.initializeConfFile("");
-			std::cout << "Using default configuration file" << std::endl;
-		}
-		else
-		{
-			config.initializeConfFile(argv[1]);
-			std::cout << "Using provided configuration file: " << argv[1] << std::endl;
-		}
-		std::cout << "Configuration initialized successfully!" << std::endl;
-		
-		// Get configuration data
-		const std::vector<ServerBlocks>& servers = config.getServers();
-		const std::vector<size_t>& ports = config.getPorts();
-
-		std::cout << "\nConfiguration Summary:" << std::endl;
-		std::cout << "Number of servers configured: " << servers.size() << std::endl;
-		
-		std::cout << "Configured ports: ";
-		for (size_t i = 0; i < ports.size(); ++i)
-		{
-			std::cout << ports[i];
-			if (i < ports.size() - 1)
-				std::cout << ", ";
-		}
-		std::cout << std::endl;
-
-		// Print each server's info
-		for (size_t i = 0; i < servers.size(); ++i)
-		{
-			std::cout << "\nServer " << i + 1 << " Configuration:" << std::endl;
-			std::map<std::string, std::string>::const_iterator it;
-			for (it = servers[i].begin(); it != servers[i].end(); ++it)
-			{
-				std::cout << "  " << it->first << ": " << it->second << std::endl;
-			}
-		}
-		
-		// Future: Set up and run servers
-
-		return (0);
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-		return (1);
-	}
+    std::string configPath = "";
+    
+    if (argc > 1) {
+        configPath = argv[1];
+    }
+    try {
+        Server server(configPath);
+        
+        server.run();
+    } 
+	catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
 }
