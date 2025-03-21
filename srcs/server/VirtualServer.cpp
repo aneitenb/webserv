8a7b3e6bd5b02dd3a7f1ce5d01f4f0a98884346f
Mils <militsaspasic@gmail.com>
2025-03-21 18:25:31 +0200

    added more things to the virtual

--- srcs/server/VirtualServer.cpp
+++ srcs/server/VirtualServer.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:26:02 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/21 18:03:52 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/03/21 18:25:06 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,37 +34,50 @@ VirtualServer::VirtualServer(ConfigurationFile _config){
     memset(&(_address.sin_zero), '\0', 8); //zero the rest of the struct
     
     if (inet_pton(AF_INET, IP, &(_address.sin_addr.s_addr)) <= 0){
-        //error
+        std::cerr << "Error: socket() failed\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
     _addr_size = sizeof(_address);
 }
 
 VirtualServer::~VirtualServer(){
     std::cout << "Virtual server destroyed\n";
+    close(_sockfd);
 }
 
 int VirtualServer::setup_fd(void){
     if (_sockfd = socket(PF_INET, SOCK_STREAM, 0) == -1){
-        //error
+        std::cerr << "Error: socket() failed\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
     //setsockopt: manipulate options for the socket 
     //CONSIDER: SO_RCVBUF / SO_SNDBUF, SO_LINGER, SO_KEEPALIVE, TCP_NODELAY
     //get socket error
     if ((setsockopt(_sockfd, SOL_SOCKET, SO_ERROR, &_sock_err, sizeof(_sock_err))) == -1){
-        //error
+        std::cerr << "Error: sersockopt() failed: SO_ERROR\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
     //reuse port, multiple bind on the same port
     int reuse_port = 1;
     if ((setsockopt(_sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port))) == -1){
-        //error
+        std::cerr << "Error: sersockopt() failed: SO_REUSEPORT\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
     //reuse address: reuse a local address or port that is in the TIME_WAIT state (e.g., after closing a socket
-    int reuse_port = 1;
-    if ((setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_port, sizeof(reuse_port))) == -1){
-        //error
+    int reuse_addr = 1;
+    if ((setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(reuse_addr))) == -1){
+        std::cerr << "Error: sersockopt() failed: SO_REUSEADDR\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
     //make it non-blocking
     if ((fcntl(_sockfd, F_SETFL, O_NONBLOCK)) == -1){
-        //error
+        std::cerr << "Error: fcntl() failed\n";
+        std::cerr << strerror(errno) << "\n";
+        return (-1);
     }
 }
